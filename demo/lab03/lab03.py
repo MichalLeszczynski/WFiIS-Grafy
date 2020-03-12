b6011544958ea254f29fccbd4a5e41d6327b6fd3
Michal Leszczynski <michal.leszczynski@aptiv.com>
2020-03-12 21:20:31 +0100

    Init setup for lab03

---
+++ demo/lab03/lab03.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import sys
+
+sys.path.insert(0, "../..")
+from spacja.simple_graph import SimpleGraph
+from spacja.graph_builder import GraphBuilder
+
+# setup
+gb = GraphBuilder()
